fea38778a8f21203515097585cb6b2469188118b
Vivek Koundal <koundalvivek073@gmail.com>
2026-08-10 12:07:54 +0530

    added extrapractice.java

--- patternpractice.java
+++ patternpractice.java
@@ -14,14 +14,24 @@ public class patternpractice {
    // }
     //System.out.println();
 //
-int n = 5;
-for(int i = 1; i<=n; i++){
-    for(int j = 1; j <= i; j++){
-        System.out.print( j+"");
+//int n = 5;
+//for(int i = 1; i<=n; i++){
+   // for(int j = 1; j <= i; j++){
+        //System.out.print( j+"");
+    //}
+    //System.out.println();}
+    //}
+    int n = 10;
+    for(int i = 1; i <= n; i++){
+for(int j = 1; j <= n-i; j++){
+    System.out.print(" ");
+}for(int k = 1; k <=n; k++){
+    System.out.print("* ");
+}System.out.println();
+}
     }
-    System.out.println();}
     }
-    }
+
 
     
 
