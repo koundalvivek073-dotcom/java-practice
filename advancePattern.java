451b6cb6452a1c2860a6408f1891a468ee354a74
vivek koundal <koundalvivek073@gmail.com>
2026-08-14 14:16:28 +0530

    added

---
+++ advancePattern.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class patternpractice {
+   public static void main(String[] var0) {
+    Scanner sc = new Scanner(System.in);
+int n =6;
+for(int i = 1; i <= 6; i++){
+for(int j =1; j <= i; j++){
+    System.out.print("*");
+}
+for(int j=1; j<= n - i +1; j++){
+    System.out.print(" ");
+}
+for(int j =1; j <= i; j++){
+    System.out.print("*");
+}System.out.println();
+}
+}
+}
+  
