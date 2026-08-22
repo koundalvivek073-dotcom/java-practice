0c16c9b36216812eec40185ea8f86d2c6581668e
Vivek Koundal <koundalvivek073@gmail.com>
2026-08-22 11:36:21 +0530

    adding vovel

---
+++ vovel.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class vovel{
+   public static void main(String[] var0) {
+Scanner sc = new Scanner(System.in);
+char input = sc.next().charAt(0);
+if(input == 'a' || input == 'e' || input == 'i' || input == 'o' || input == 'u'){
+    System.out.println("vovel");
+}
+else{System.out.print("conso");
+}
+
+}
+
+
+   }
